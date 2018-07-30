3e56a26d9871489b03c2b96a3fd361d1d6a61728
hitscs <sunchensong@hotmail.com>
2018-07-30 08:46:33 +0800

    获取openID

---
+++ service/WeChatPay/script/GetOpenId.groovy
@@ -0,0 +1,27 @@
+
+import java.util.Map
+
+// org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger("findParty")
+
+def getOpenIdUrl="https://api.weixin.qq.com/sns/jscode2session"
+def appid=""
+def secret=""
+def js_code=code
+def grant_type="authorization_code"
+ExecutionContext ec = context.ec
+
+
+String url=getOpenIdUrl+"?appid="+appid+"&secret="+secret+"&js_code="+js_code+"&grant_type="+grant_type
+
+org.moqui.util.RestClient.RestResponse openIdResponse = ec.service.rest().method("GET").uri(url).call()
+if (openIdResponse.statusCode == 200) {
+
+	Map respMap = (Map) openIdResponse.jsonObject()
+	
+	
+} else {
+	ec.message.addMessage("Error ")
+}
+
+
+
